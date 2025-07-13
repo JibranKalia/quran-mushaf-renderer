cfe9300babff6bb813044780c5682a32aaaf7715
Jibran Kalia <jibran.kalia@hey.com>
2025-07-13 18:02:44 -0500

    Add PageViewModel

--- app.rb
+++ app.rb
@@ -53,16 +53,133 @@ class Chapter
   end
 end
 
+# View Models
+class PageViewModel
+  attr_reader :mushaf_id, :page_number, :mushaf, :lines
+
+  def initialize(mushaf_id, page_number)
+    @mushaf_id = mushaf_id
+    @page_number = page_number
+    @mushaf = Mushaf.find(mushaf_id)
+    @lines = prepare_lines
+  end
+
+  def next_page
+    page_number + 1
+  end
+
+  def prev_page
+    page_number - 1
+  end
+
+  private
+
+  def prepare_lines
+    raw_lines = MushafPage.for_page(mushaf_id, page_number)
+
+    raw_lines.map do |line|
+      LineViewModel.new(line, mushaf)
+    end
+  end
+end
+
+class LineViewModel
+  attr_reader :number, :type, :is_centered, :data
+
+  def initialize(line, mushaf)
+    @number = line['line_number']
+    @type = line['line_type']
+    @is_centered = line['is_centered'] == 1
+    @data = prepare_data(line, mushaf)
+  end
+
+  def css_classes
+    classes = ['line']
+    classes << 'line--center' if is_centered
+    classes << "line--#{type.gsub('_', '-')}" if type
+    classes.join(' ')
+  end
+
+  private
+
+  def prepare_data(line, mushaf)
+    case type
+    when 'surah_name'
+      prepare_surah_data(line)
+    when 'basmallah'
+      { text: '﷽' }
+    when 'ayah'
+      prepare_ayah_data(line, mushaf)
+    else
+      {}
+    end
+  end
+
+  def prepare_surah_data(line)
+    return {} unless line['surah_number']
+
+    chapter = Chapter.find(line['surah_number'])
+    return {} unless chapter
+
+    {
+      number: line['surah_number'],
+      formatted_number: "surah#{line['surah_number'].to_s.rjust(3, '0')}",
+      chapter: chapter
+    }
+  end
+
+  def prepare_ayah_data(line, mushaf)
+    words = Word.find_range(mushaf, line['first_word_id'], line['last_word_id'])
+
+    # Group words by ayah
+    ayahs = []
+    current_ayah = nil
+    current_words = []
+
+    words.each do |word|
+      if word['ayah'] != current_ayah
+        if current_ayah
+          ayahs << {
+            surah: word['surah'],
+            ayah: current_ayah,
+            words: current_words
+          }
+        end
+        current_ayah = word['ayah']
+        current_words = []
+      end
+
+      current_words << {
+        id: word['id'],
+        text: word['text'],
+        location: word['location'],
+        is_end: word['text']&.match?(/\u06DD/)
+      }
+    end
+
+    # Don't forget the last ayah
+    if current_ayah && !current_words.empty?
+      ayahs << {
+        surah: words.last['surah'],
+        ayah: current_ayah,
+        words: current_words
+      }
+    end
+
+    { ayahs: ayahs }
+  end
+end
+
 # Routes
 get '/' do
-  redirect '/mushaf/1/page/1'
+  redirect '/mushaf/1/page/15'
 end
 
 get '/mushaf/:mushaf_id/page/:page_number' do
-  @mushaf_id = params[:mushaf_id].to_i
-  @page_number = params[:page_number].to_i
-  @mushaf = Mushaf.find(@mushaf_id)
-  @lines = MushafPage.for_page(@mushaf_id, @page_number)
+  @page_data = PageViewModel.new(
+    params[:mushaf_id].to_i,
+    params[:page_number].to_i
+  )
 
   erb :page
 end
