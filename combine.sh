9e94815b4542cb98991d270043c57d44b2faa8f8
Jibran Kalia <jibran.kalia@hey.com>
2025-07-14 07:33:40 -0500

    Add hiz manzil rub ruku data

--- combine.sh
+++ combine.sh
@@ -1,12 +1,10 @@
 DEST=quran-combined.db
-rm -f "$DEST" # wipe any previous run
 
 for db in \
-  qpc-v1-15-lines.db \
-  qpc-v1-glyph-codes-wbw.db \
-  quran-metadata-ayah.db \
-  quran-metadata-surah-name.db \
-  quran-metadata-juz.db; do
-  # Dump each source and pipe into the destination
+  quran-metadata-hizb.db \
+  quran-metadata-manzil.db \
+  quran-metadata-rub.db \
+  quran-metadata-ruku.db \
+  quran-metadata-sajda.db; do
   sqlite3 "$db" .dump | sqlite3 "$DEST"
 done
